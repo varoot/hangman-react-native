62a9c460d8f8f9d9a3e97f65b33de47082bb7428
Varoot Phasuthadol <varoot@gmail.com>
2021-08-12 16:36:25 -0400

    Add swinging / update animation so it animates when clearing the drawing

--- components/HangmanSvg.tsx
+++ components/HangmanSvg.tsx
@@ -1,10 +1,11 @@
 import React, { memo, useEffect, useRef } from 'react';
 import { useCallback } from 'react';
-import { Animated } from 'react-native';
+import { Animated, Easing } from 'react-native';
 import { Colors } from 'react-native-paper';
 import Svg, { Circle, G, Line } from 'react-native-svg';
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedG = Animated.createAnimatedComponent(G);
 const AnimatedLine = Animated.createAnimatedComponent(Line);
 
 const animationDuration = 250;
@@ -24,6 +25,7 @@ function Hangman(props: HangmanProps): JSX.Element {
   const { stage = 0 } = props;
 
   const stepAnim = useRef(new Animated.Value(0)).current;
+  const swingAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     Animated.timing(stepAnim, {
@@ -31,7 +33,56 @@ function Hangman(props: HangmanProps): JSX.Element {
       duration: animationDuration,
       useNativeDriver: true,
     }).start();
-  }, [stage, stepAnim]);
+
+    if (stage >= 10) {
+      Animated.sequence([
+        Animated.delay(animationDuration),
+        Animated.loop(
+          Animated.sequence([
+            Animated.timing(swingAnim, {
+              toValue: 1,
+              duration: 500,
+              easing: Easing.out(Easing.sin),
+              useNativeDriver: false,
+            }),
+            Animated.timing(swingAnim, {
+              toValue: -1,
+              duration: 1000,
+              easing: Easing.inOut(Easing.sin),
+              useNativeDriver: false,
+            }),
+            Animated.timing(swingAnim, {
+              toValue: 0,
+              duration: 500,
+              easing: Easing.in(Easing.sin),
+              useNativeDriver: false,
+            }),
+          ]),
+        ),
+      ]).start();
+    } else {
+      swingAnim.stopAnimation();
+      Animated.timing(swingAnim, {
+        toValue: 0,
+        duration: animationDuration,
+        useNativeDriver: false,
+      }).start();
+    }
+  }, [stage, stepAnim, swingAnim]);
+
+  const getStroke = useCallback(
+    (baseStage: number) => {
+      return {
+        ...lineProps,
+        opacity: stepAnim.interpolate({
+          extrapolate: 'clamp',
+          inputRange: [baseStage, baseStage + 0.01],
+          outputRange: [0, 1],
+        }),
+      };
+    },
+    [stepAnim],
+  );
 
   const getValue = useCallback(
     (baseStage: number, outputRange: [number, number] | [string, string]) => {
@@ -46,36 +97,33 @@ function Hangman(props: HangmanProps): JSX.Element {
 
   return (
     <Svg height="200" viewBox="0 0 200 200" width="200">
-      {stage > 0 && <AnimatedLine x1="2" x2={getValue(0, [2, 198])} y1="198" y2="198" {...lineProps} />}
-      {stage > 1 && <AnimatedLine x1="30" x2="30" y1="198" y2={getValue(1, [198, 2])} {...lineProps} />}
-      {stage > 2 && <AnimatedLine x1="30" x2={getValue(2, [30, 150])} y1="2" y2="2" {...lineProps} />}
+      <AnimatedLine x1="2" x2={getValue(0, [2, 198])} y1="198" y2="198" {...getStroke(0)} />
+      <AnimatedLine x1="30" x2="30" y1="198" y2={getValue(1, [198, 2])} {...getStroke(1)} />
+      <AnimatedLine x1="30" x2={getValue(2, [30, 150])} y1="2" y2="2" {...getStroke(2)} />
       <G transform="translate(130 2)">
-        {stage > 3 && <AnimatedLine x1="0" x2="0" y1="0" y2={getValue(3, [0, 38])} {...lineProps} />}
-        {stage > 4 && (
+        <AnimatedG
+          transform={swingAnim.interpolate({
+            inputRange: [-1, 1],
+            outputRange: ['rotate(-2)', 'rotate(2)'],
+          })}
+        >
+          <AnimatedLine x1="0" x2="0" y1="0" y2={getValue(3, [0, 38])} {...getStroke(3)} />
           <G transform="translate(0 58)">
             <AnimatedCircle
               cx="0"
               cy="0"
               r="20"
               strokeDasharray={getValue(4, ['0 200', `${20 * Math.PI * 2} 200`])}
-              {...lineProps}
               transform="rotate(-90)"
+              {...getStroke(4)}
             />
           </G>
-        )}
-        {stage > 5 && <AnimatedLine x1="0" x2="0" y1="78" y2={getValue(5, [78, 128])} {...lineProps} />}
-        {stage > 6 && (
-          <AnimatedLine x1="0" x2={getValue(6, [0, -30])} y1="78" y2={getValue(6, [78, 108])} {...lineProps} />
-        )}
-        {stage > 7 && (
-          <AnimatedLine x1="0" x2={getValue(7, [0, 30])} y1="78" y2={getValue(7, [78, 108])} {...lineProps} />
-        )}
-        {stage > 8 && (
-          <AnimatedLine x1="0" x2={getValue(8, [0, -30])} y1="128" y2={getValue(8, [128, 158])} {...lineProps} />
-        )}
-        {stage > 9 && (
-          <AnimatedLine x1="0" x2={getValue(9, [0, 30])} y1="128" y2={getValue(9, [128, 158])} {...lineProps} />
-        )}
+          <AnimatedLine x1="0" x2="0" y1="78" y2={getValue(5, [78, 128])} {...getStroke(5)} />
+          <AnimatedLine x1="0" x2={getValue(6, [0, -30])} y1="78" y2={getValue(6, [78, 108])} {...getStroke(6)} />
+          <AnimatedLine x1="0" x2={getValue(7, [0, 30])} y1="78" y2={getValue(7, [78, 108])} {...getStroke(7)} />
+          <AnimatedLine x1="0" x2={getValue(8, [0, -30])} y1="128" y2={getValue(8, [128, 158])} {...getStroke(8)} />
+          <AnimatedLine x1="0" x2={getValue(9, [0, 30])} y1="128" y2={getValue(9, [128, 158])} {...getStroke(9)} />
+        </AnimatedG>
       </G>
     </Svg>
   );
